774171092d010658215316cee5ed22bc50c24d46
Samantha Radecki <samantha.radecki@gmail.com>
2021-08-19 17:12:14 -0500

    added googleKey

--- client/components/SignUp.js
+++ client/components/SignUp.js
@@ -3,8 +3,6 @@ import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
 import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
@@ -57,7 +55,7 @@ export default function SignUp() {
           <LockOutlinedIcon />
         </Avatar>
         <pwa-auth
-          googlekey="..."
+          googlekey="562995514162-fa154hmkk4n670oelf3mj49bqu0qomts.apps.googleusercontent.com"
           facebookkey="..."
           windowskey = "..."
         ></pwa-auth>
