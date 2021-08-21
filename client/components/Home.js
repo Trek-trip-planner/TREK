e481b049f5bd150941a9dd1b9b6471fbbb1d5fc5
Allison Collier <10acollier@gmail.com>
2021-08-20 23:08:55 -0400

    added logged in menu options to navbar, create pagination in all parks page, and added cards to home page

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,50 +1,88 @@
-import React, { useState } from 'react'
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
 import { CssBaseline } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
-import Button from '@material-ui/core/Button';
 import HomeCarousel from './ImgCarousel';
 import IndividualPark from './IndividualPark';
 import history from '../history';
+import { fetchParksThunk } from '../store/parks';
+import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     minHeight: '100vh',
     backgroundImage: '/ZionNationalPark.jpg',
     backgroundRepeat: 'no-repeat',
-    backgroundSize: 'cover'
+    backgroundSize: 'cover',
   },
   contain: {
     disableGutters: 'false',
     padding: 0,
-    maxWidth: 'unset'
-  }
+    maxWidth: 'unset',
+  },
+  title: {
+    cursor: 'pointer',
+    align: 'center',
+    fontWeight: 'fontWeightBold',
+    padding: 20,
+  },
 }));
 
-function Home () {
+function Home(props) {
+  useEffect(() => {
+    (async () => {
+      await props.getParks();
+    })();
+  }, []);
+
   const classes = useStyles();
-  // const [parks] = useState()
+
+  if (!props.parks) {
+    return 'Loading';
+  }
+
+  const sliceStart = Math.floor(Math.random() * props.parks.length);
+  const sliceEnd = sliceStart + 6;
 
   return (
-    <div >
-      <CssBaseline />
-        <Container className={classes.contain} >
-          <HomeCarousel />
-        </Container>
-        <Container align='center'>
-          <Button onClick={() => history.push("/all-parks")} >
-            Explore All Parks
-          </Button>
-        </Container>
-        {/* <Grid container spacing={3}>
-          {parks.map((park) => (
-            <Grid item key={park.id} xs={12} md={6} lg={4} >
-              <IndividualPark park={park}/>
-            </Grid>
-          ))}
-        </Grid> */}
+    <div>
+      <Container className={classes.contain}>
+        <HomeCarousel />
+      </Container>
+      <Container align='center'>
+        <Typography
+          className={classes.title}
+          variant='h4'
+          component='h3'
+          color='primary'
+          onClick={() => history.push('/all-parks')}
+        >
+          Explore All Parks
+        </Typography>
+      </Container>
+      <Grid container spacing={3}>
+        {props.parks.slice(sliceStart, sliceEnd).map((park) => (
+          <Grid item key={park.id} xs={12} md={6} lg={4}>
+            <IndividualPark park={park} />
+          </Grid>
+        ))}
+      </Grid>
     </div>
-  )
+  );
 }
 
-export default Home;
+const mapState = (state) => {
+  return {
+    parks: state.parks,
+  };
+};
+
+const mapDispatch = (dispatch) => {
+  return {
+    getParks: () => dispatch(fetchParksThunk()),
+  };
+};
+
+export default connect(mapState, mapDispatch)(Home);
