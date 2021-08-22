bb52803c2fa5f3efef199950743d0be338c692d6
Samantha Radecki <samantha.radecki@gmail.com>
2021-08-22 16:06:20 -0500

    all 400 parks render dropdown

--- client/components/Searchbar.js
+++ client/components/Searchbar.js
@@ -1,15 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import { connect } from 'react-redux';
 import SearchIcon from '@material-ui/icons/Search';
-import InputBase from '@material-ui/core/InputBase';
+
 import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
+import { fetchParksThunk } from '../store/parks';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles((theme) => ({
   search: {
     display: 'flex',
     position: 'relative',
-    backgroundColor: 'white',
     borderRadius: theme.shape.borderRadius,
     marginRight: theme.spacing(2),
     marginLeft: 0,
@@ -25,8 +27,8 @@ const useStyles = makeStyles((theme) => ({
     padding: theme.spacing(0, 1.5),
     display: 'flex',
     borderRadius: theme.shape.borderRadius,
-    backgroundColor: theme.palette.secondary.main,
-    height: '100%',
+    background: theme.palette.secondary.main,
+    height: '70%',
     position: 'absolute',
     pointerEvents: 'none',
     display: 'flex',
@@ -48,61 +50,53 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-function Searchbar() {
+function Searchbar(props) {
   const classes = useStyles();
   const [searchValue, setSearchValue] = useState('');
 
-  const handleSearchChange = (e) => {
+  useEffect(() => {
+    (async () => {
+      const parks = await props.getParks();
+    })();
+  }, []);
+
+  function handleSearchValue(e) {
+    console.log('event.target.value', e.target.value);
     setSearchValue(e.target.value);
-  };
+  }
+
+  function handleClick(searchValue) {
+    console.log('searchValue', searchValue);
+  }
 
   return (
     <div className={classes.search}>
-      <div className={classes.searchIcon}>
-        <SearchIcon />
-      </div>
       <Autocomplete
+        onChange={handleSearchValue}
         className={classes.inputInput}
-        options={top25Parks}
-        getOptionLabel={(option) => option.name}
+        options={props.parks}
+        getOptionLabel={(park) => park.fullName}
         style={{ width: 300 }}
-        renderInput={(park) => (
-          <TextField
-            {...park}
-            onChange={handleSearchChange}
-            variant='outlined'
-          />
-        )}
+        renderInput={(park) => <TextField {...park} variant='outlined' />}
       />
+      <Button onClick={() => handleClick(searchValue)}>
+        <div className={classes.searchIcon}>
+          <SearchIcon />
+        </div>
+      </Button>
     </div>
   );
 }
-const top25Parks = [
-  { name: 'Olympic National Park' },
-  { name: 'Mount Rainier National Park' },
-  { name: 'John Day Fossil Beds National Monument' },
-  { name: 'Crater Lake National Park' },
-  { name: 'Yosemite National Park' },
-  { name: 'Sequoia & Kings Canyon National Parks' },
-  { name: 'Death Valley National Park' },
-  { name: 'Joshua Tree National Park' },
-  { name: 'Glacier National Park' },
-  { name: 'Yellowstone National Park' },
-  { name: 'Grand Teton National Park' },
-  { name: 'Rocky Mountain National Park ' },
-  { name: 'Great Sand Dunes National Park & Preserve' },
-  { name: 'Mesa Verde National Park' },
-  { name: 'Arches National Park' },
-  { name: 'Capitol Reef National Park' },
-  { name: 'Bryce Canyon National Park' },
-  { name: 'Zion National Park' },
-  { name: 'Grand Canyon National Park' },
-  { name: 'Canyon De Chelly National Monument' },
-  { name: 'Saguaro National Park' },
-  { name: 'White Sands National Monument' },
-  { name: 'Guadalupe Mountains National Park' },
-  { name: 'Big Bend National Park' },
-  { name: 'Scotts Bluff National Monument' },
-];
+const mapState = (state) => {
+  return {
+    parks: state.parks,
+  };
+};
 
-export default Searchbar;
+const mapDispatch = (dispatch) => {
+  return {
+    getParks: () => dispatch(fetchParksThunk()),
+  };
+};
+
+export default connect(mapState, mapDispatch)(Searchbar);
