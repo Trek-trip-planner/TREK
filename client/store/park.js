ea5acb4e08b7a17ea5cb5ec0b7cc85c54439a033
Allison Collier <10acollier@gmail.com>
2021-08-24 16:02:53 -0400

    made map render

--- client/store/park.js
+++ client/store/park.js
@@ -1,10 +1,15 @@
 import axios from 'axios';
 
 const GET_PARK = 'GET_PARK';
+const CLEAR_PARK = 'CLEAR_PARK';
 
 const getPark = (park) => ({
   type: GET_PARK,
   park,
+});
+
+export const clearPark = () => ({
+  type: CLEAR_PARK,
 });
 
 export const fetchParkThunk = (parkName) => {
@@ -24,6 +29,8 @@ export default function parkReducer(state = initialState, action) {
   switch (action.type) {
     case GET_PARK:
       return action.park;
+    case CLEAR_PARK:
+      return {};
     default:
       return state;
   }
