2b85a138377975fc5c9cc404c55c53a649dbf9b8
nelasunitha <nelasunitha@gmail.com>
2021-08-24 19:22:58 -0700

    moving

--- client/secret.js
+++
@@ -1,4 +0,0 @@
-export const MB_PUBKEY =
-  'pk.eyJ1Ijoic2FtcmFkZWNraTAwMyIsImEiOiJja3NnbTJjMmQxbGxnMndwYTZnOXU5c3MyIn0.7hEc6AdwRA2mHrK-lTOvIw';
-export const MB_PRIKEY =
-  'sk.eyJ1Ijoic2FtcmFkZWNraTAwMyIsImEiOiJja3Nob3dhenoxd2xwMm5ybzh5cHY3MTYzIn0.WOC3HimPh1DqZY5BP_Um9w';
